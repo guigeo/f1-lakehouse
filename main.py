754ad6f70b907217e101a3eb82a5926bc992fd04
Guilherme Ramos <uepa.pap@gmail.com>
2026-03-05 16:18:10 -0300

    subindo ultimas alteracoes

--- main.py
+++ main.py
@@ -4,24 +4,26 @@ import time
 
 import dotenv
 
-from collect import CollectResults
-from sender import Sender
+from src.collect import CollectResults
+from src.sender import Sender
 
 dotenv.load_dotenv()
 
 BUCKET_NAME = os.getenv("BUCKET_NAME")
+BUCKET_FOLDER = os.getenv("BUCKET_FOLDER")
 
-while True:
+#while True:
 
-    print("Iniciando processo...")
+print("Iniciando processo...")
 
-    print("Coletando dados....")
-    collect_data = CollectResults(years=[datetime.datetime.now().year])
-    collect_data.process_years()
+print("Coletando dados....")
+#collect_data = CollectResults(years=[datetime.datetime.now().year])
+collect_data = CollectResults(years=[2019])
+collect_data.process_years()
 
-    print("Enviando dados...")
-    sender_data = Sender(bucket_name=BUCKET_NAME, bucket_folder="f1/results")
-    sender_data.process_folder("data/")
+print("Enviando dados...")
+sender_data = Sender(bucket_name=BUCKET_NAME, bucket_folder=BUCKET_FOLDER)
+sender_data.process_folder("data/")
 
-    print("Iteração finalizada.")
-    time.sleep(60*60*6)
+print("Iteração finalizada.")
+#time.sleep(60*60*6)
