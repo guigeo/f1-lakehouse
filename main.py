75078440f71a07ce3cc7b825fd70f0ba0873967d
Guilherme Ramos <uepa.pap@gmail.com>
2026-03-05 19:11:27 -0300

    ajuste main elegante

--- main.py
+++ main.py
@@ -15,11 +15,18 @@ BUCKET_FOLDER = os.getenv("BUCKET_FOLDER")
 print("Iniciando processo...")
 
 print("Coletando dados....")
-collect_data = CollectResults(years=[datetime.datetime.now().year])
-#collect_data = CollectResults(years=[2020,2021,2022,2023,2024,2025])
+#collect_data = CollectResults(years=[datetime.datetime.now().year])
+collect_data = CollectResults(years=[2025])
 collect_data.process_years()
 
-print("Enviando dados...")
+files = [f for f in os.listdir("data") if f.endswith(".parquet")]
+
+if not files:
+    print("⚠️ Nenhum dado encontrado. Nada será enviado para o S3.")
+    exit()
+
+print(f"✅ {len(files)} arquivo(s) encontrado(s). Iniciando envio para o S3...")
+
 sender_data = Sender(bucket_name=BUCKET_NAME, bucket_folder=BUCKET_FOLDER)
 sender_data.process_folder("data/")
 
